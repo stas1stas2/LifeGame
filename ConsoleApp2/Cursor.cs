d5ce5dbb3be8d1c15a5ca1705340d547399f4644
ss.savenko <ss.savenko@gmail.com>
2018-05-11 21:50:30 +0300

    bug fixed

--- ConsoleApp2/Cursor.cs
+++ ConsoleApp2/Cursor.cs
@@ -39,7 +39,7 @@
             }
             set
             {
-                if (value > 0 && maxYPosition > value)
+                if (value >= 0 && maxYPosition > value)
                 {
                     yPosition = value;
                 }
